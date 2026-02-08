14038636b4d7d7e3282e0d4556d74cbf6e3ff229
Naveen Badiger <83811961+Naveenub@users.noreply.github.com>
2026-02-08 18:51:18 +0530

    Create thresholds.py

---
+++ backend/app/core/thresholds.py
@@ -0,0 +1,7 @@
+THRESHOLDS = {
+    "temperature": {"warning": 50, "critical": 70},
+    "humidity": {"warning": 80, "critical": 90},
+    "smoke": {"warning": 300, "critical": 500},
+    "co": {"warning": 35, "critical": 70},
+    "flame": {"critical": 1}
+}
